e5cdc4294691d5a0f5d96679252632c01f50f5c6
d.bitter <daveybitter@gmail.com>
2017-05-08 18:29:03 +0200

    generating stand data

--- generator/app.js
+++ generator/app.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express')
 const app = require('express')()
 const port = 1337
 
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MongoClient = require("mongodb").MongoClient
+const MONGODB_URI = process.env.MONGODB_URI
 console.log(MONGODB_URI)
 
 MongoClient.connect(MONGODB_URI, (err, database) => {
 	if (err) return console.log(err)
 	db = database
-});
+})
 
 
 app
@@ -25,58 +25,71 @@ app
 app.get('/init/generator/:generator', (req, res) => {
 	const generator = req.params.generator
 	initGenerator(generator)
-});
+})
 
 app.get('/init/generator/:generator/stand/:stand', (req, res) => {
 	const generator = req.params.generator
 	const stand = req.params.stand
 	initStand(generator, stand)
-});
+})
 
 app.get('/grid/:id', (req, res) => {
 
-});
+})
 
 app.get('/generator/:id', (req, res) => {
 
-});
+})
+
+app.get('/stand/:id', (req, res) => {
+	const standCollection = db.collection('stands')
+
+
+	standCollection.findOne({
+		name: stand
+	}, function(err, stand) {
+		if (err) return console.log(err)
+
+
+	})
+})
 
 const generate = () => {
 	setInterval(function() {
-		const generatorCollection = db.collection('generators');
-		const standCollection = db.collection('stands');
+		const generatorCollection = db.collection('generators')
+		const standCollection = db.collection('stands')
 
 		standCollection.find({}, {}).toArray(function(err, stands) {
 			stands.map(function(stand) {
 				generateMessage(stand)
-			});
+			})
 
-		});
-	}, 3000);
+		})
+	}, 3000)
 }
 
 generate()
 
 const initGenerator = (generator) => {
-	const generatorCollection = db.collection('generators');
+	const generatorCollection = db.collection('generators')
 	const data = {
 		name: generator,
 		created_at: Date.now()
 	}
 
 	generatorCollection.save(data, (err, result) => {
-		if (err) return console.log(err);
-	});
+		if (err) return console.log(err)
+	})
 }
 
 const initStand = (generator, stand) => {
-	const generatorCollection = db.collection('generators');
-	const standCollection = db.collection('stands');
+	const generatorCollection = db.collection('generators')
+	const standCollection = db.collection('stands')
 
 	generatorCollection.findOne({
 		name: generator
 	}, function(err, generator) {
-		if (err) return console.log(err);
+		if (err) return console.log(err)
 		const data = {
 			name: stand,
 			generator: generator._id,
@@ -84,16 +97,16 @@ const initStand = (generator, stand) => {
 		}
 
 		standCollection.save(data, (err, result) => {
-			if (err) return console.log(err);
+			if (err) return console.log(err)
 			console.info('Created stand called `' + data.name + '` that is connected to generator `' + generator.name + '`')
-		});
+		})
 	})
 
 }
 
 const generateMessage = (stand) => {
-	const generatorCollection = db.collection('generators');
-	const messageCollection = db.collection('messages');
+	const generatorCollection = db.collection('generators')
+	const messageCollection = db.collection('messages')
 
 	const data = {
 		stand: stand._id,
@@ -105,11 +118,11 @@ const generateMessage = (stand) => {
 	}
 
 	messageCollection.save(data, (err, result) => {
-		if (err) return console.log(err);
+		if (err) return console.log(err)
 		console.info('Create message for stand `' + stand.name + '` that is connected to generator `' + stand.generator + '`')
-	});
+	})
 }
 
 const randomNumnodem = (min, max) => {
-	return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min)) + min
 }
