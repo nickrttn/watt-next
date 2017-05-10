4efae95a925b3bc2473766a07c0b00316d0cfd48
Nick Rutten <hello@nickrttn.me>
2017-05-10 14:52:42 +0200

    PR comments and added browserify setup

--- routes/index.js
+++ routes/index.js
@@ -5,31 +5,30 @@ const router = express.Router();
 const request = require('request');
 const moment = require('moment');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-	const io = req.app.locals.settings.io;
-	// 
-	// io.on('connection', function(socket) {
-	// 	console.log('connected ' + socket.id)
-	// 	setInterval(() => {
-	// 		getData(io)
-	// 	}, 1000);
-	// })
+module.exports = (io) => {
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+		// io.on('connection', function(socket) {
+		// 	console.log('connected ' + socket.id)
+		// 	setInterval(() => {
+		// 		getData(io)
+		// 	}, 1000);
+		// })
 
-	res.render('index', {
-		title: 'Watt-Next'
+		res.render('index', {
+			title: 'Watt-Next'
+		});
 	});
-});
 
-const getData = (io) => {
-	const url = 'http://localhost:1337/api/v1/stand/craftbeers/messages'
-	request(url, function(err, response, body) {
-		let data = JSON.parse(body);
-		data.messages = data.messages.slice(data.messages.length - 6, data.messages.length - 1);
-		data.messages[4].time = moment(	data.messages[4].timestamp).startOf('hour').fromNow();
+	const getData = (io) => {
+		const url = 'http://localhost:1337/api/v1/stand/craftbeers/messages'
+		request(url, (err, response, body) => {
+			let data = JSON.parse(body);
+			data.messages = data.messages.slice(data.messages.length - 6, data.messages.length - 1);
+			data.messages[4].time = moment(	data.messages[4].timestamp).startOf('hour').fromNow();
+			io.emit('updated data', data)
+		});
+	}
 
-		io.emit('updated data', data)
-	})
-}
-
-module.exports = router;
+	return router;
+};
