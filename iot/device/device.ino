dff4b8b38839019e1b6bf8e60a843888253ad7b5
Nick Rutten <hello@nickrttn.me>
2017-05-10 14:17:00 +0200

    started work on wifi connection

--- iot/device/device.ino
+++ iot/device/device.ino
@@ -1,9 +1,32 @@
+#include <ESP8266WiFi.h>
+
+const char ssid = "HvA Open Wi-Fi";
+const char password = "";
+
+
+const int pot = A0;
+int read_out = 0;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+  	delay(500);
+  	Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+	read_out = analogRead(pot);
+	Serial.println(read_out);
 }
